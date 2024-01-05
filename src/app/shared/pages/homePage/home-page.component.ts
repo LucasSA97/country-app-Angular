ad5941227fb4a4e7f0cc008eec905d93e3c16047
Lucas <lucassarachu7@gmail.com>
2024-01-05 11:21:09 +0100

    Routing app countries

---
+++ src/app/shared/pages/homePage/home-page.component.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-home-page',
+  template: `<p>homePage works!</p>`,
+  styles: []
+})
+export class HomePageComponent { }
