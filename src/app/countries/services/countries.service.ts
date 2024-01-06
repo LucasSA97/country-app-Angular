b7c73a576a26db85e62a8f37f58f63e7d21b4f3a
Lucas <lucassarachu7@gmail.com>
2024-01-06 20:15:53 +0100

    Manejo de errores en observables

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of, tap } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -8,11 +8,33 @@ export class CountriesService {
 
   private apiUrl: string = 'https://restcountries.com/v3.1'
 
+  // private apiUrlCountries : string = 'https://restcountries.com/v3.1/name/'
+
+  // https://restcountries.com/v3.1/region/{region}
+
   constructor( private http: HttpClient ) { }
 
   searchCapital( term: string ) : Observable<Country[]>  {
+
    const url = `${ this.apiUrl }/capital/${ term }`
    return this.http.get<Country[]>( url )
+    .pipe( catchError( () => of([]))
+   )
+  }
+
+  searchCountry( term: string) : Observable<Country[]> {
+
+    const url = `${ this.apiUrl }/name/${ term }`
+   return this.http.get<Country[]>( url )
+    .pipe( catchError( () => of([]))
+   )
+  }
+
+  searchRegion( region: string) : Observable<Country[]> {
+    const url = `${ this.apiUrl }/region/${ region }`
+    return this.http.get<Country[]>( url )
+     .pipe( catchError( () => of([]))
+    )
   }
 
 }
