37d7272d4d1ecd4a12fe787c5720f0dfb4e47bf9
Lucas <lucassarachu7@gmail.com>
2024-01-10 14:42:24 +0100

    Manteniendo los datos en todos los components

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
@@ -8,12 +8,18 @@ import { Country } from '../../interfaces/country';
   styleUrl: './by-country-page.component.css'
 })
 
-export class ByCountryPageComponent {
+export class ByCountryPageComponent implements OnInit {
 
   public countries: Country[] = []
   public isLoading: boolean = false
-  constructor( private countriesService: CountriesService ) {
+  public initialValue : string = ''
 
+  constructor( private countriesService: CountriesService ) {
+  }
+
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byCountries.countries
+    this.initialValue = this.countriesService.cacheStore.byCountries.term
   }
 
   searchByCountries ( term: string ) : void {
