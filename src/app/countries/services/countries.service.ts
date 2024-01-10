01b3dcb0c535fd8af9b9b8c4b1d95f29e1febcfa
Lucas <lucassarachu7@gmail.com>
2024-01-10 11:48:50 +0100

    Manteniendo la data

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,25 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, delay, map, of, tap } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
+
 import { Country } from '../interfaces/country';
+import { CacheStore } from '../interfaces/cache-store.interface';
 
 @Injectable({providedIn: 'root'})
 export class CountriesService {
 
   private apiUrl: string = 'https://restcountries.com/v3.1'
 
-  // private apiUrlCountries : string = 'https://restcountries.com/v3.1/name/'
+  public cacheStore : CacheStore = {
+    byCapital: { term: '', countries: [] },
+    byCountries: { term: '', countries: [] },
+    byRegion: { region: '', countries: [] },
+  }
 
-  // https://restcountries.com/v3.1/region/{region}
+  constructor( private http: HttpClient ) {
 
+  }
   private getCountriesRequest( url : string) : Observable<Country[]>{
     return this.http.get<Country[]>(url)
       .pipe( catchError( () => of([])),
       // delay( 2000 ),
       )
   }
-
-  constructor( private http: HttpClient ) { }
 
   searchCountryById( code: string ) : Observable<Country | null> {
 
