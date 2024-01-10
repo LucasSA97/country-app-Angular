e6fe956553ee8a4834306f5de8f6523273299577
Lucas <lucassarachu7@gmail.com>
2024-01-10 12:32:48 +0100

    Persistencia de datos 2, term en la searchbox

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, map, of } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 
 import { Country } from '../interfaces/country';
 import { CacheStore } from '../interfaces/cache-store.interface';
@@ -40,7 +40,9 @@ export class CountriesService {
   searchCapital( term: string ) : Observable<Country[]>  {
 
    const url = `${ this.apiUrl }/capital/${ term }`
-   return this.getCountriesRequest(url)
+   return this.getCountriesRequest(url).pipe(
+    tap( countries => this.cacheStore.byCapital = { term, countries} )
+   )
   }
 
   searchCountry( term: string) : Observable<Country[]> {
--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -12,6 +12,8 @@ export class SearchBoxComponent implements OnInit, OnDestroy {
 
   @Input()
   public placeholder : string = ''
+  @Input()
+  public initialValue : string = ''
 
   @Output()
   public onValue = new EventEmitter<string>()
